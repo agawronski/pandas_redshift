3f59c9054cc8f0fda9f682afa34e656cb18163b8
Aidan Gawronski <aidangawronski@gmail.com>
2017-07-30 18:09:10 -0400

    updating download url.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name = 'pandas_redshift',
+    packages = ['pandas_redshift'],
     version = '0.1',
-    scripts = ['pandas_redshift'],
+    description = 'Load data from redshift into a pandas DataFrame and vice versa.',
     author = 'Aidan Gawronski',
     author_email = 'aidangawronski@gmail.com',
     url = 'https://github.com/agawronski/pandas_redshift',
+    download_url = 'https://github.com/agawronski/pandas_redshift/archive/1.0.tar.gz', # I'll explain this in a second
     install_requires = ['traceback',
                         'psycopg2',
                         'pandas',
