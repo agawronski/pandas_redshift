50ac63dfc84fa1161236d0371dd3dfa1d98c54a4
Aidan Gawronski <aidangawronski@gmail.com>
2017-08-07 20:45:42 -0400

    Adding a check that DataFrame columns are not Redshift reserve words.

--- pandas_redshift/core.py
+++ pandas_redshift/core.py
@@ -52,60 +52,64 @@ def pandas_to_redshift(data_frame,
                         quotechar = '"',
                         dateformat = 'auto',
                         timeformat = 'auto'):
-    csv_name = redshift_table_name + '.csv'
-    if save_local == True:
-        data_frame.to_csv(csv_name, index = index, sep = delimiter)
-        print('saved file {0} in {1}'.format(csv_name, os.getcwd()))
-        data_send = open(csv_name, 'rb')
-        s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = data_send)
-        print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
+    rrwords = pd.read_csv('docs/redshift_reserve_words.txt')
+    not_valid = [r for r in data_frame.columns if r in rrwords]
+    if not_valid:
+        print('DataFrame column name {0} is a reserve word in redshift'.format(not_valid[0]))
     else:
-        csv_buffer = StringIO()
-        data_frame.to_csv(csv_buffer, index = index, sep = delimiter)
-        s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = csv_buffer.getvalue())
-        print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
-    # CREATE AN EMPTY TABLE IN REDSHIFT
-    if index == True:
-        columns = list(data_frame.columns)
-
-        if data_frame.index.name:
-            columns.insert(0, data_frame.index.name)
+        csv_name = redshift_table_name + '.csv'
+        if save_local == True:
+            data_frame.to_csv(csv_name, index = index, sep = delimiter)
+            print('saved file {0} in {1}'.format(csv_name, os.getcwd()))
+            data_send = open(csv_name, 'rb')
+            s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = data_send)
+            print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
         else:
-            columns.insert(0, "index")
-    else:
-        columns = list(data_frame.columns)
-    if column_data_types is None:
-        column_data_types = ['varchar(256)'] * len(columns)
-    columns_and_data_type = ', '.join(['{0} {1}'.format(x, y) for x,y in zip(columns, column_data_types)])
-    create_table_query = 'create table {0} ({1})'.format(redshift_table_name, columns_and_data_type)
-    print(create_table_query)
-    print('CREATING A TABLE IN REDSHIFT')
-    cursor.execute('drop table if exists {0}'.format(redshift_table_name))
-    cursor.execute(create_table_query)
-    connect.commit()
-    # CREATE THE COPY STATEMENT TO SEND FROM S3 TO THE TABLE IN REDSHIFT
-    bucket_name = 's3://{0}/{1}'.format(s3_bucket_var, s3_subdirectory_var + csv_name)
-    s3_to_sql = """
-    copy {0}
-    from '{1}'
-    delimiter '{2}'
-    ignoreheader 1
-    csv quote as '{3}'
-    dateformat '{4}'
-    timeformat '{5}'
-    access_key_id '{6}'
-    secret_access_key '{7}';
-    """.format(redshift_table_name, bucket_name, delimiter, quotechar, dateformat, timeformat, aws_1, aws_2)
-    print(s3_to_sql)
-    # send the file
-    print('FILLING THE TABLE IN REDSHIFT')
-    try:
-        cursor.execute(s3_to_sql)
+            csv_buffer = StringIO()
+            data_frame.to_csv(csv_buffer, index = index, sep = delimiter)
+            s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = csv_buffer.getvalue())
+            print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
+        # CREATE AN EMPTY TABLE IN REDSHIFT
+        if index == True:
+            columns = list(data_frame.columns)
+            if data_frame.index.name:
+                columns.insert(0, data_frame.index.name)
+            else:
+                columns.insert(0, "index")
+        else:
+            columns = list(data_frame.columns)
+        if column_data_types is None:
+            column_data_types = ['varchar(256)'] * len(columns)
+        columns_and_data_type = ', '.join(['{0} {1}'.format(x, y) for x,y in zip(columns, column_data_types)])
+        create_table_query = 'create table {0} ({1})'.format(redshift_table_name, columns_and_data_type)
+        print(create_table_query)
+        print('CREATING A TABLE IN REDSHIFT')
+        cursor.execute('drop table if exists {0}'.format(redshift_table_name))
+        cursor.execute(create_table_query)
         connect.commit()
-    except Exception as e:
-        print(e)
-        traceback.print_exc(file=sys.stdout)
-        connect.rollback()
+        # CREATE THE COPY STATEMENT TO SEND FROM S3 TO THE TABLE IN REDSHIFT
+        bucket_name = 's3://{0}/{1}'.format(s3_bucket_var, s3_subdirectory_var + csv_name)
+        s3_to_sql = """
+        copy {0}
+        from '{1}'
+        delimiter '{2}'
+        ignoreheader 1
+        csv quote as '{3}'
+        dateformat '{4}'
+        timeformat '{5}'
+        access_key_id '{6}'
+        secret_access_key '{7}';
+        """.format(redshift_table_name, bucket_name, delimiter, quotechar, dateformat, timeformat, aws_1, aws_2)
+        print(s3_to_sql)
+        # send the file
+        print('FILLING THE TABLE IN REDSHIFT')
+        try:
+            cursor.execute(s3_to_sql)
+            connect.commit()
+        except Exception as e:
+            print(e)
+            traceback.print_exc(file=sys.stdout)
+            connect.rollback()
 
 
 def close_up_shop():
