1b2de270e6c48eab3a9cc365b7c3feceeecabcb4
Aidan Gawronski <aidangawronski@gmail.com>
2017-09-20 11:38:33 -0400

    adding a license to the new version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'pandas_redshift',
     packages = ['pandas_redshift'],
-    version = '1.0.9',
+    version = '1.1.0',
     description = 'Load data from redshift into a pandas DataFrame and vice versa.',
     author = 'Aidan Gawronski',
     author_email = 'aidangawronski@gmail.com',
