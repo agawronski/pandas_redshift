c79ff4439c19ee1da079300b72cebefe7412c8c5
Eric Lo <lxneng@gmail.com>
2018-12-28 11:31:53 +0800

    PEP8: improvements style

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name = 'pandas_redshift',
-    packages = ['pandas_redshift'],
-    version = '1.1.3',
-    description = 'Load data from redshift into a pandas DataFrame and vice versa.',
-    author = 'Aidan Gawronski',
-    author_email = 'aidangawronski@gmail.com',
+    name='pandas_redshift',
+    packages=['pandas_redshift'],
+    version='1.1.3',
+    description='Load data from redshift into a pandas DataFrame and vice versa.',
+    author='Aidan Gawronski',
+    author_email='aidangawronski@gmail.com',
     # url = 'https://github.com/agawronski/pandas_redshift',
-    python_requires = '>=3',
-    install_requires = ['psycopg2-binary',
-                        'pandas',
-                        'boto3'],
-    include_package_data = True
+    python_requires='>=3',
+    install_requires=['psycopg2-binary',
+                      'pandas',
+                      'boto3'],
+    include_package_data=True
 )
