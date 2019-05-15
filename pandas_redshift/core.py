ef494b94a080e46173ab75f14ca1a5382385e249
mdarblade <matthias.darblade@gmail.com>
2019-05-15 15:47:05 -0700

    Change if to elif

--- pandas_redshift/core.py
+++ pandas_redshift/core.py
@@ -116,7 +116,7 @@ def df_to_s3(data_frame, csv_name, index, save_local, delimiter, **kwargs):
 def pd_dtype_to_redshift_dtype(dtype):
     if dtype.startswith('int64'):
         return 'BIGINT'
-    if dtype.startswith('int'):
+    elif dtype.startswith('int'):
         return 'INTEGER'
     elif dtype.startswith('float'):
         return 'REAL'
